bd9ff411a09414157b069f60b7a2ae14ff6483ff
billiestone21 <billiebstone@outlook.com>
2024-10-03 15:39:40 -0600

    HW 02 report - update to remove 999 outlier

--- Code/Stone_project_01.R
+++ Code/Stone_project_01.R
@@ -2,6 +2,12 @@
 
 rawdat <- read.csv("DataRaw/lead-iq-01.csv")
 
+clean_dat <- rawdat
+
+clean_dat[clean_dat$IQ == 999, "IQ"] <- 99
+write.csv(clean_dat, "DataProcessed/lead-iq-01.clean.csv",
+          row.names = FALSE)
+
 boxplot(IQ ~ Smelter,
         data=rawdat,
         main = "Boxplot of IQ Scores by Smelter Group", 
